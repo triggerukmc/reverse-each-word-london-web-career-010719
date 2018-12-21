a843752799de2345a2bf37fddf6600d3a1ce6f4c
Matthew Collins <matthew.collins1991@yahoo.com>
2018-12-21 16:51:24 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,8 @@
 def reverse_each_word(string)
 array = []
 array = string.split(" ")
-r_string = array.collect do |x| x.reverse
-end
+r_string = array.each do |x| x.reverse
 r_string.join(" ")
+end
+
 end
